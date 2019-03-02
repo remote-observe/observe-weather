d95899e038f45c238d46337c307b2a8b2aec4619
Bret Little <bret.little@gmail.com>
2019-03-02 00:35:11 +0000

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # observe-weather
 A plugin for remote-observer that interfaces with a weather station
+
+
+This controller parses a weather file of the format:
+
+```
+temperature=64
+dewTemperature=42
+windSpeed=7.345
+windDirection=284.0425
+pressure=29.96
+rainFall=0.000
+```
+
+# Setup
+Inside the observe-client config, add the controller:
+
+```js
+module.exports = {
+  firebase: {
+    // ...
+  },
+  controllers: [
+    {
+      type: 'weather',
+      package: 'observe-weather',
+      filePath: '/an/absolute/observe-weather/example-weather-file.txt',
+    }
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+exports.getStatus = async function(config) {
+  let weatherFile;
+
+  try {
+    weatherFile = await readFile(config.filePath);
+  } catch (error) {
+    throw new Error(`Cannot read file: ${config.filePath}`);
+  }
+
+  const lines = weatherFile.split('\n').map(line => line.split('='));
+
+  return lines.reduce(
+    (output, line) => ({
+      ...output,
+      [line[0]]: line[1],
+    }),
+    {},
+  );
+};
+
+function readFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+}
