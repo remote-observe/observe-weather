af316f04a7bcfc6a7846c4c42efdf6519d928472
Bret Little <bret.little@gmail.com>
2019-04-17 00:48:27 +0000

    Initial implementation

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ exports.getStatus = async function(config) {
 
   const lines = weatherFile.split('\n').map(line => line.split('='));
 
-  return lines.reduce(
+  return lines.filter(line => line[0]).reduce(
     (output, line) => ({
       ...output,
       [line[0]]: line[1],
